1e6e5e583d30cc244ccb9a3acc70adf7f49dae07
ezequiel stom <ezequielstom@gmail.com>
2024-08-25 19:37:11 -0300

    Actualizar componente de tooltip para mostrar información adicional en la barra de navegación

---
+++ src/components/ui/SimpleGallery/SimpleGallery.jsx
@@ -0,0 +1,44 @@
+import { useEffect } from "react";
+import PhotoSwipeLightbox from "photoswipe/lightbox";
+import "photoswipe/style.css";
+
+export function SimpleGallery(props) {
+	useEffect(() => {
+		let lightbox = new PhotoSwipeLightbox({
+			gallery: "#" + props.galleryID,
+			children: "a",
+			pswpModule: () => import("photoswipe"),
+		});
+		lightbox.init();
+
+		return () => {
+			lightbox.destroy();
+			lightbox = null;
+		};
+	}, []);
+
+	return (
+		<div className="pswp-gallery my-12  " id={props.galleryID}>
+			{props.images.map((image, index) => (
+				<a
+					href={image.largeURL}
+					data-pswp-width={image.width}
+					data-pswp-height={image.height}
+					data-cropped="true"
+					key={props.galleryID + "-" + index}
+					target="_blank"
+					rel="noreferrer"
+					className="">
+					<h3 className="my-1">{image.title}</h3>
+					<div className="shadow-xl rounded-md">
+						<img
+							src={image.thumbnailURL}
+							alt={image.title || `Image ${index + 1}`}
+							className="rounded-sm"
+						/>
+					</div>
+				</a>
+			))}
+		</div>
+	);
+}
